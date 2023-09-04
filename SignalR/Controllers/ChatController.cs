8cc166d1fcdc41e593955c0ae4106ed87fbbe506
Alex <yuting09120310@gmail.com>
2023-09-04 17:22:05 +0800

    初次上傳

---
+++ SignalR/Controllers/ChatController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SignalR.Models;
+using System.Diagnostics;
+
+namespace SignalR.Controllers
+{
+	public class ChatController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public ChatController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult ChatRoom()
+		{
+			return View();
+		}
+
+	}
+}
